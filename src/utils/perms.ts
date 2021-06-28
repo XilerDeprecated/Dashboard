d12d6a132f7f5f70cad88dffd14199ba4f99f4d5
Arthurdw <mail@arthurdw.com>
2021-06-28 15:20:21 +0200

    :memo: Added some more code docs.

--- src/utils/perms.ts
+++ src/utils/perms.ts
@@ -1,7 +1,22 @@
+/**
+ * A bitwise representation of all user permissions.
+ */
 export enum Permissions {
-  MEMBER = 0x00,
+  /** Represents a banned member, this is the lowest level. */
+  BANNED = 0x00,
+
+  /** Represents a member, this is the standard permission level. */
+  MEMBER = 0x02,
+
+  /** Represents a member of our support team, which should allow them to perfom more actions. */
   SUPPORT = MEMBER | 0x32,
+
+  /** Represents a member of our moderation team, which should allow them to perform most actions. */
   MODERATOR = SUPPORT | 0x64,
+
+  /** Represents a member of our administration team, which should allow then to perform almost all actions. */
   ADMIN = MODERATOR | 0x128,
+
+  /** Represents a member of our management team, which should allow them to perform every action. */
   MANAGEMENT = ADMIN | 0x256,
 }
--- src/utils/requests.ts
+++ src/utils/requests.ts
@@ -1,10 +1,27 @@
 import { API_BASE_URL } from "./config";
 
-export const fetcher = (nput: RequestInfo, init?: RequestInit | undefined) =>
+/**
+ * Fetch something from the API, this method gets used by SWR.
+ *
+ * @param nput The request url.
+ * @param init The request props, credentials is on "include".
+ * @returns The json response as an object from the request.
+ */
+export const fetcher = (
+  nput: RequestInfo,
+  init?: RequestInit | undefined
+): object =>
   fetch(nput, { ...init, credentials: "include" }).then((res) => res.json());
 
+/**
+ * Signs out the current user.
+ *
+ * @returns An error if one got thrown.
+ */
 export const logout = () =>
   fetch(`${API_BASE_URL}/logout`, {
     method: "DELETE",
     credentials: "include",
   }).catch((e) => e);
+
+// TODO: Make custom method for all request types and handle then properly.
