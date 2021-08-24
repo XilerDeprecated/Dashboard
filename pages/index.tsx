179aad7ab6e8d5129977e0993acdf0d5d67dca12
Arthurdw <mail@arthurdw.com>
2021-08-24 22:00:03 +0200

    :bug: Fixed bug which prevents chart from loading.

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,14 +1,19 @@
+import React, { useEffect, useState } from "react";
+
 import { Activity } from "@components/Activity.comp";
 import { Balance } from "@components/Balance.comp";
 import { MostUsedApps } from "@components/MostUsedApps.comp";
-import React from "react";
 
 export const Main: React.FC = () => {
+  const [showActivity, setShowActivity] = useState(false);
+
+  useEffect(() => setShowActivity(true), []);
+  
   return (
     <div className="grid gap-6 lg:grid-cols-fix-left lg:grid-rows-fix-top lg:grid-flow-col">
       <Balance />
       <MostUsedApps />
-      <Activity />
+      {showActivity && <Activity />}
     </div>
   );
 };
